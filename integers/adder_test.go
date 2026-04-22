eedae1898353a3c1710adc1a08fb5db8917e0a47
Hamfit <opefawazademolar@gmail.com>
2026-04-22 17:47:22 +0100

    .

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -4,11 +4,6 @@ import (
 	"fmt"
 	"testing"
 )
-
-func Add(x, y int) int {
-	return x + y 	
-}
-
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
 	expected := 4 
@@ -21,6 +16,7 @@ func TestAdder(t *testing.T) {
 func ExampleAdd() {
 	sum := Add(1, 5)
 	fmt.Println(sum)
+	// output: 6
 }
 
 
