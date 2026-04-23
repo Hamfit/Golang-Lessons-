d677b8290b900edae355e2a2d1eab222de4b7744
Hamfit <opefawazademolar@gmail.com>
2026-04-23 17:46:49 +0100

    .

--- Iteration/repeat.go
+++ Iteration/repeat.go
@@ -1,11 +1,13 @@
 package iteration
 
+import "strings"
+
 const repeatCount = 5
 
 func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated += character
+	var repeated strings.Builder
+	for range 5 {
+		repeated .WriteString(character)
 	}
-	return repeated
+	return repeated.String()
 }
