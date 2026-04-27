f7bc99f1e70b6ab8649d727e9c4a9bb1d499111e
Hamfit <opefawazademolar@gmail.com>
2026-04-27 17:16:36 +0100

    .

---
+++ recoding-exercises/countalpha.go
@@ -0,0 +1,21 @@
+// package main
+
+// // import (
+// // 	"fmt"
+// // 	"unicode"
+// // )
+
+// func countAlpha(s string) int {
+// 	count := 0
+// 	for _, char := range s {
+// 		if unicode.IsLetter(char) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// // func main() {
+// // 	str := "Hello 123!"
+// // 	fmt.Println(countAlpha(str))
+// // }
