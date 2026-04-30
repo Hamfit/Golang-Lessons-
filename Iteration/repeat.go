971fd05ba212d82bd7a519374fe37523f6a069b2
techAlhaji <ademolabuild@gmail.com>
2026-04-30 17:38:08 +0100

    .

--- Iteration/repeat.go
+++ Iteration/repeat.go
@@ -7,7 +7,7 @@ const repeatCount = 5
 func Repeat(character string) string {
 	var repeated strings.Builder
 	for range 5 {
-		repeated .WriteString(character)
+		repeated.WriteString(character)
 	}
 	return repeated.String()
 }
